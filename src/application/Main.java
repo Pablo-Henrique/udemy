8c18f864219889fa59f203d9ef6b1e9a0306c58b
Pablo Henrique <pabloh.anselmo@gmail.com>
2021-06-07 16:40:33 -0300

    Mensagem de boa noite

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,9 +1,8 @@
 package application;
-
 public class Main {
-
     public static void main(String[] args) {
         System.out.println("Bom dia");
         System.out.println("Boa Tarde");
+        System.out.println("Boa Noite");
     }
 }
