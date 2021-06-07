1918712428f0431e5b604f416b4007f049b4e971
Pablo Henrique <pabloh.anselmo@gmail.com>
2021-06-07 16:30:26 -0300

    Acrescentada mensagem de boa Tarde

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,6 +3,7 @@ package application;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Bom dia");
+        System.out.println("Boa Tarde");
     }
 }
